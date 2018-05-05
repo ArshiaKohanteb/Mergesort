87a7cbbcf6b55928369975bf94ffcaac31525de3
Arshia <arshiakohanteb@gmail.com>
2018-05-04 17:32:03 -0700

    Starting

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -8,9 +8,39 @@ namespace MergeSort
 {
     class Program
     {
-        static void Main(string[] args)
+        static int[] Helper(int[] nums)
+        {
+            bool even = false;
+            return MergeSort(nums,even)
+        }
+        static int[] MergeSort(int[] nums, bool even)
+        {
+            if (nums % 2 == 0)
+            {
+                even = true;
+            }
+            int[] left;
+            int[] right;
+            //split it into 2 arrays
+            if (even)//Checks if even
+            {
+                
+            }
+            else
+            {
+
+            }
+            return Merge(MergeSort(left), MergeSort(right));
+        }
+
+        static int[] Merge(int[] left, int[] right)
         {
 
+        }
+
+        static void Main(string[] args)
+        {
+            
         }
     }
 }
