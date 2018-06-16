509e478a42f17555762659950a96457ba82b8a55
Arshia <arshiakohanteb@gmail.com>
2018-06-15 17:29:44 -0700

    Donegit add .git add .!

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -13,6 +13,11 @@ namespace MergeSort
             //split it into 2 arrays
             //Checks if even
 
+            if (nums.Length == 1)
+            {
+                return nums;
+            }
+
             int[] left = new int[nums.Length / 2];
             int[] right = new int[nums.Length / 2];
 
@@ -33,47 +38,117 @@ namespace MergeSort
                 right[i] = nums[numsIndex];
             }
 
-            if (left.Length == 1)
-            {
-                return left;
-            }
+            int[] leftResult = MergeSort(left);
+            int[] rightResult = MergeSort(right);
 
-            return Merge(MergeSort(left), MergeSort(right));
+            return Merge(leftResult, rightResult);
         }
 
         static int[] Merge(int[] left, int[] right)
-        {
+        {   // Loop until any input array has been completely copied
+
+            // Copy the rest of left
+
+            // copy the rest of right
             int lefti = 0;
             int righti = 0;
-            int index= 0;
+            int index = 0;
 
             int[] nums = new int[left.Length + right.Length];
 
-            bool merged = false;
-
-            while(merged == false)
+            while(lefti < left.Length && righti < right.Length)
+            {
+                if (left[lefti] < right[righti])
+                {
+                    nums[index] = right[righti];
+                    righti++;
+                }
+                else
+                {
+                    nums[index] = left[lefti];
+                    lefti++;
+                }
+                index++;
+            }    
+            
+            while(righti < right.Length)
+            {
+                nums[index] = right[righti];
+                index++;
+                righti++;
+            }
+            while (lefti < left.Length)
+            {
+                nums[index] = left[lefti];
+                index++;
+                lefti++;
+            }
+            /*
+            while (!merged)
             {
                 if (left[lefti] < right[righti])
                 {
                     nums[index] = left[lefti];
                     lefti++;
-                    index++;
                 }
                 else
                 {
                     nums[index] = right[righti];
                     righti++;
-                    index++;
+                }
+
+                index++;
+
+                if (righti >= right.Length)
+                {
+                    merged = true;
                 }
             }
 
+            for (int i = index; i < nums.Length; i++, lefti++)
+            {
+                nums[i] = left[lefti];
+            }*/
+            /*
+            while (!merged)
+            {
+                if(righti >= right.Length || left[lefti] < right[righti])
+                {
+                    right[righti] = nums[index];
+                    righti++;
+                }
+                else if (lefti >= left.Length || left[lefti] > right[righti])
+                {
+                    left[lefti] = nums[index];
+                    lefti++;
+                }
+
+                int number = 0;
+                for(int i = 0; i < nums.Length;i++)
+                {
+                    if (nums[i] != 0)
+                    {
+                        number++;
+                    }
+                }
+                if (number == nums.Length)
+                    merged = true;
+
+                index++;
+            }
+            */
             return nums;
         }
 
         static void Main(string[] args)
         {
-            int[] nums = { 1, 2, 3, 4 };
-            nums = MergeSort(nums);
+            int[] nums = {100,200,300,400,500};
+            int[] final = MergeSort(nums);
+            for(int i = 0; i < final.Length;i++)
+            {
+                Console.WriteLine(final[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
